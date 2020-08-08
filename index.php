fcff64260bbd5d0cc643afd4be9ea0564a8f0bab
ma0613 <45188801+ma0613@users.noreply.github.com>
2020-08-08 17:52:01 +0900

    Update index.php
    For Test

--- index.php
+++ index.php
@@ -5,10 +5,23 @@
     </head>
     <body>
         <form action="./userinfo.php?mode=test" method="POST">
+            <p>테스트</p>
             <!--<p>아이디 : <input type="varchar" name="u_id"></p>
             <p>현재 비밀번호 : <input type="varchar" name="pw"></p>
             <p>변경 비밀번호 : <input type="varchar" name="new_pw"></p>-->
             <!-- <p>비밀번호 : <textarea name="description" id="" cols="30" rows="10"></textarea></p> -->
+            <p><input type="submit" /></p>
+        </form>
+        <form action="./userinfo.php?mode=join" method="POST">
+            <p>회원가입</p>
+            <p>아이디 : <input type="varchar" name="u_id"></p>
+            <p>비밀번호 : <input type="varchar" name="pw"></p>
+            <p><input type="submit" /></p>
+        </form>
+        <form action="./userinfo.php?mode=delete" method="POST">
+            <p>탈퇴</p>
+            <p>아이디 : <input type="varchar" name="u_id"></p>
+            <p>비밀번호 : <input type="varchar" name="pw"></p>
             <p><input type="submit" /></p>
         </form>
     </body>
