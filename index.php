376b6576a28ebffb7e14d8e5a4f4603070219232
ma0613 <45188801+ma0613@users.noreply.github.com>
2020-08-08 16:15:08 +0900

    Test

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-    </head>   
+    </head>
     <body>
-        <form action="./userinfo.php?mode=modify" method="POST">
+        <form action="./userinfo.php?mode=join" method="POST">
             <p>아이디 : <input type="varchar" name="u_id"></p>
             <p>현재 비밀번호 : <input type="varchar" name="pw"></p>
-            <p>변경 비밀번호 : <input type="varchar" name="new_pw"></p> 
+            <p>변경 비밀번호 : <input type="varchar" name="new_pw"></p>
             <!-- <p>비밀번호 : <textarea name="description" id="" cols="30" rows="10"></textarea></p> -->
-            <p><input type="submit" /></p>            
+            <p><input type="submit" /></p>
         </form>
     </body>
 </html>
