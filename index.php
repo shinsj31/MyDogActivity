551b910c05ff5b061eaed499f77c4ea8fd65278c
Joy <shinsj31@naver.com>
2020-08-08 16:10:43 +0900

    [Joy] Git push first
    - push .php files into git rep.
    
    Signed-off-by: Joy <shinsj31@naver.com>

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>   
+    <body>
+        <form action="./userinfo.php?mode=modify" method="POST">
+            <p>아이디 : <input type="varchar" name="u_id"></p>
+            <p>현재 비밀번호 : <input type="varchar" name="pw"></p>
+            <p>변경 비밀번호 : <input type="varchar" name="new_pw"></p> 
+            <!-- <p>비밀번호 : <textarea name="description" id="" cols="30" rows="10"></textarea></p> -->
+            <p><input type="submit" /></p>            
+        </form>
+    </body>
+</html>
---
+++ userinfo.php
@@ -0,0 +1,145 @@
+﻿<?php
+
+try{
+ $option = array(
+     PDO::MYSQL_ATTR_FOUND_ROWS => true,
+     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION    //에러출력 옵션 : 에러출력
+ );
+ 
+ $dbh = new PDO('mysql:host=localhost;dbname=mydogactivity', 'root', '10qpalzm' , $option);
+}
+catch(Exception $e) {
+ echo$e->getMessage();
+}
+
+
+switch($_GET['mode']){
+/*
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);  // 에러 출력하지 않음
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // Warning만 출력
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // 에러 출력
+
+    회원 가입 용 / 
+    생성, 비밀번호 변경, 탈퇴
+*/
+    case 'login':
+        $stmt = $dbh->prepare('SELECT * FROM user WHERE u_id = :u_id AND pw = :pw ');
+        $stmt->bindParam(':u_id',$u_id);
+        $stmt->bindParam(':pw',$pw);
+ 
+        $u_id = $_POST['u_id'];
+        $pw = $_POST['pw'];
+
+        try {
+            $stmt->execute();
+          
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+		}
+        
+       
+        $count = $stmt->rowCount();
+        
+        if($count>0){
+       
+            echo 'success';
+  
+        }else{
+            echo 'false';
+        }
+
+        break;
+    case 'join': // 가입
+        $stmt = $dbh->prepare('INSERT INTO user (u_id, pw) VALUES (:u_id, :pw)');
+        $stmt->bindParam(':u_id',$u_id);
+        $stmt->bindParam(':pw',$pw);
+ 
+        $u_id = $_POST['u_id'];
+        $pw = $_POST['pw'];
+
+        try {
+            $stmt->execute();
+           
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+		}
+
+
+        $count = $stmt->rowCount();
+       
+        if($count>0){
+            echo 'success';
+  
+        }else{
+            echo 'false';
+        }
+
+        
+
+        //header("Location: list.php"); 
+
+        break;
+    case 'delete': // 탈퇴
+        $stmt = $dbh->prepare('DELETE FROM user WHERE u_id = :u_id AND pw = :pw ');
+        $stmt->bindParam(':u_id',$u_id);
+        $stmt->bindParam(':pw',$pw);
+ 
+        $u_id = $_POST['u_id'];
+        $pw = $_POST['pw'];
+
+
+        try {
+            $stmt->execute();
+           
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+		}
+
+        $count = $stmt->rowCount();
+       
+        if($count>0){
+            echo 'success';
+  
+        }else{
+            echo 'false';
+        }
+
+        //header("Location: list.php"); 
+        break;
+    case 'modify': // 비밀번호 변경
+        $stmt = $dbh->prepare('UPDATE user SET pw = :new_pw WHERE u_id = :u_id AND pw = :pw');
+        $stmt->bindParam(':u_id', $u_id);
+        $stmt->bindParam(':pw', $pw);
+        $stmt->bindParam(':new_pw', $new_pw);
+ 
+        $u_id = $_POST['u_id'];
+        $pw = $_POST['pw'];
+        $new_pw = $_POST['new_pw'];
+
+        try {
+            $stmt->execute();
+           
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+		}
+       
+        $count = $stmt->rowCount();
+       
+        if($count>0){
+            echo 'success';
+  
+        }else{
+            echo 'false';
+        }
+
+        //header("Location: list.php?id={$_POST['id']}");
+        break;
+}
+?>
