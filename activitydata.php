38a0976ae58f65fbbeeedc45571942c3cc9cfb34
ma0613 <45188801+ma0613@users.noreply.github.com>
2020-09-20 11:14:25 +0900

    Update activitydata.php
    to test date type

--- activitydata.php
+++ activitydata.php
@@ -35,7 +35,7 @@ switch($_GET['mode']){
 
         if($count>0){
           $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-         
+
           echo json_encode($list ,  JSON_UNESCAPED_UNICODE);
         }
         else echo 'false';
@@ -60,7 +60,7 @@ switch($_GET['mode']){
 
         if($count>0){
           $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-         
+
           echo json_encode($list ,  JSON_UNESCAPED_UNICODE);
         }
         else echo 'false';
@@ -70,7 +70,7 @@ switch($_GET['mode']){
          $stmt = $dbh->prepare('SELECT * FROM activity_info WHERE d_id = :d_id ORDER BY ac_id DESC limit 1');
         $stmt->bindParam(':d_id',$d_id);
         $d_id = $_POST['d_id'];
-    
+
         try {
             $stmt->execute();
         }
@@ -82,17 +82,17 @@ switch($_GET['mode']){
 
         if($count>0){
           $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
-         
+
           echo json_encode($list ,  JSON_UNESCAPED_UNICODE);
         }
         else echo 'false';
 
         break;
     case 'add': // 활동 정보 추가
-        $stmt = $dbh->prepare('INSERT INTO activity_info 
-        (d_id , ac_date , ac_hour, ac_minute , ac_walk, ac_run , ac_distance , ac_heart_rate, ac_location ,ac_device_id ) 
+        $stmt = $dbh->prepare('INSERT INTO activity_info
+        (d_id , ac_date , ac_hour, ac_minute , ac_walk, ac_run , ac_distance , ac_heart_rate, ac_location ,ac_device_id )
         VALUES (:d_id , :ac_date , :ac_hour, :ac_minute , :ac_walk, :ac_run , :ac_distance , :ac_heart_rate, :ac_location ,:ac_device_id )');
-       
+
         $stmt->bindParam(':d_id',$d_id);
         $stmt->bindParam(':ac_date',$ac_date);
         $stmt->bindParam(':ac_hour',$ac_hour);
@@ -105,9 +105,9 @@ switch($_GET['mode']){
         $stmt->bindParam(':ac_device_id',$ac_device_id);
 
         $d_id = $_POST['d_id'];
-        $ac_date = $_POST['ac_date'];
-        $ac_hour = $_POST['ac_hour'];
-        $ac_minute = $_POST['ac_minute'];
+        $ac_date =  date( "Y-m-d", timestamp = time()); //$_POST['ac_date'];
+        $ac_hour =  date( "H", timestamp = time()); //$_POST['ac_hour'];
+        $ac_minute =  date( "i", timestamp = time()); // $_POST['ac_minute'];
         $ac_walk = $_POST['ac_walk'];
         $ac_run = $_POST['ac_run'];
         $ac_distance = $_POST['ac_distance'];
@@ -128,11 +128,9 @@ switch($_GET['mode']){
 
 
         break;
-      
+
 
 
 }
 
 ?>
-  
-        
